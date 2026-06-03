d89116555460bf8b4088a059d19fb2e5295a4374
Al Mahmud <alma.mahmud.dev@gmail.com>
2026-06-03 17:44:43 +0600

    Mahmud (#5)
    * Navber and typography somg add
    
    * Footer some change
    
    * HeroSection,TrustMetrics, and reuseable component sectionHerader add
    
    ---------
    
    Co-authored-by: DevOnionX <devonionx@gmail.com>

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,12 @@
-import HeroSection from "@/components/sections/HeroSection";
+import HeroSection from '@/components/sections/HeroSection'
+import TrustMetrics from '@/components/sections/TrustMetrics'
+
 
 export default function page() {
   return (
     <>
-      <HeroSection />
+    <HeroSection/>
+    <TrustMetrics/>
     </>
   );
 }
