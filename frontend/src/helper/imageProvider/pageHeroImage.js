5351bc48517455fde01231775958e75d219cf858
Sree Jhulon Kumar Ray <jhulonkumar40@gmail.com>
2026-06-09 19:44:27 +0600

    Feat/about jk (#27)
    feat: added PageHero in  About page

--- frontend/src/helper/imageProvider/pageHeroImage.js
+++ frontend/src/helper/imageProvider/pageHeroImage.js
@@ -2,17 +2,15 @@ import PageHeroImg from "../../../public/images/hero/code.jpg";
 import WorkHeroImg from "../../../public/images/hero/work-banner.jpg";
 import ServicesHeroImg from "../../../public/images/hero/services-banner.jpg";
 import BlogHeroImg from "../../../public/images/hero/blog-banner.jpg";
+import AboutHeroImg from "../../../public/images/hero/about-banner.jpg";
 
 export const allPageHeroImg = {
   pageHero: PageHeroImg,
   // for work
-  workPage: [
-    { id: 1, img: WorkHeroImg, alt: "work Page Hero Banner" },
-  ],
+  workPage: [{ id: 1, img: WorkHeroImg, alt: "work Page Hero Banner" }],
   servicePage: [
     { id: 1, img: ServicesHeroImg, alt: "services Page Hero Banner" },
   ],
-  blogPage: [
-    { id: 1, img: BlogHeroImg, alt: "Blog Page Hero Banner" },
-  ],
+  blogPage: [{ id: 1, img: BlogHeroImg, alt: "Blog Page Hero Banner" }],
+  aboutPage: [{ id: 1, img: AboutHeroImg, alt: "About Page Hero Banner" }],
 };
