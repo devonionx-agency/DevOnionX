9b2ed594e75aa3c884f0afa8ae432f8680fa191e
jihadcodes <jihad.dev.pro@gmail.com>
2026-06-11 12:24:18 +0600

    setup about page structure

--- frontend/src/app/(marketing)/about/page.jsx
+++ frontend/src/app/(marketing)/about/page.jsx
@@ -1,10 +1,23 @@
 import PageHero from "@/components/common/PageHero";
 import { aboutPageHeroData } from "@/helper/pageHero";
 
+import AboutStory from "@/components/sections/about/AboutStory";
+import AboutMission from "@/components/sections/about/AboutMission";
+import AboutTeam from "@/components/sections/about/AboutTeam";
+ 
+export const metadata = {
+  title: "About — DevonionX",
+  description: "We're a team of obsessive builders, strategic thinkers, and relentless problem-solvers.",
+};
+ 
+
 const About = () => {
   return (
     <>
       <PageHero  {...aboutPageHeroData}/>
+       <AboutStory />
+      <AboutMission />
+      <AboutTeam />
     </>
   );
 };
---
+++ frontend/src/components/sections/about/AboutMission.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutMission = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AboutMission
