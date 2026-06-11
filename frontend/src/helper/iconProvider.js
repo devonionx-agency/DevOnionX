46dd3757a8011f6723068f288d4a0604ce9939e6
DevOnionX <devonionx@gmail.com>
2026-06-11 23:56:03 +0600

    feat: added serviceHero component in projects

--- frontend/src/helper/iconProvider.js
+++ frontend/src/helper/iconProvider.js
@@ -1,6 +1,15 @@
 import { IoMdTime } from "react-icons/io";
-
+import { GoArrowUpRight } from "react-icons/go";
+import { SiNextdotjs } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
 
 export const allIcon = {
   timer: <IoMdTime />,
+  upperArrow: <GoArrowUpRight />,
+  serviceHeroCommonTech: [
+    { id: 1, icon: <SiNextdotjs />, class: "text-[#fff]" },
+    { id: 2, icon: <SiTypescript />, class: "text-[#3178C6]" },
+    { id: 3, icon: <FaReact />, class: "text-[#61DAFB]" },
+  ],
 };
---
+++ frontend/src/helper/imageProvider/serviceHeroImages.js
@@ -0,0 +1,5 @@
+import ServiceCommonImg from "../../../public/images/serviceHero/service-common.png";
+
+export const allServiceHeroImages = {
+  serviceCommonImg: ServiceCommonImg,
+};
